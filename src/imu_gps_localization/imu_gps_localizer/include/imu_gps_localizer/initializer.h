bca630603c41a2a7dbe3f6234df4244ea13c97c4
binghe <binghe@orbbec.com>
2023-02-20 11:52:09 +0800

    update

--- src/imu_gps_localization/imu_gps_localizer/include/imu_gps_localizer/initializer.h
+++ src/imu_gps_localization/imu_gps_localizer/include/imu_gps_localizer/initializer.h
@@ -7,23 +7,23 @@
 namespace ImuGpsLocalization
 {
 
-    constexpr int kImuDataBufferLength = 100;
-    constexpr int kAccStdLimit = 3.;
+constexpr int kImuDataBufferLength = 100;
+constexpr int kAccStdLimit = 3.;
 
-    class Initializer
-    {
-    public:
-        Initializer(const Eigen::Vector3d &init_I_p_Gps);
+class Initializer
+{
+public:
+    Initializer(const Eigen::Vector3d &init_I_p_Gps);
 
-        void AddImuData(const ImuDataPtr imu_data_ptr);
+    void AddImuData(const ImuDataPtr imu_data_ptr);
 
-        bool AddGpsPositionData(const GpsPositionDataPtr gps_data_ptr, State *state);
+    bool AddGpsPositionData(const GpsPositionDataPtr gps_data_ptr, State *state);
 
-    private:
-        bool ComputeG_R_IFromImuData(Eigen::Matrix3d *G_R_I);
+private:
+    bool ComputeG_R_IFromImuData(Eigen::Matrix3d *G_R_I);
 
-        Eigen::Vector3d init_I_p_Gps_;
-        std::deque<ImuDataPtr> imu_buffer_;
-    };
+    Eigen::Vector3d init_I_p_Gps_;
+    std::deque<ImuDataPtr> imu_buffer_;
+};
 
 } // namespace ImuGpsLocalization
